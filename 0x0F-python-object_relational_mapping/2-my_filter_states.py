05778190ea4a8d49c679ae536743ecaa956d8382
samuel Adeyinka <tboysammy@gmail.com>
2024-04-26 15:33:12 +0100

    sysql

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,18 +1,22 @@
 #!/usr/bin/python3
-"""A script that lists all states
-from the database hbtn_0e_0_usa
+
 """
+A script that lists all states
+from the database hbtn_0e_0_usa
+starting with N
+"""
+import MySQLdb
+import sys
 
 if __name__ == '__main__':
-    import MySQLdb
-    import sys
     arg = sys.argv
     db = MySQLdb.connect(host="localhost", port=3306, user=arg[1],
             passwd=arg[2], db=arg[3])
     cur = db.cursor()
-    cur.execute('SELECT * FROM states')
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}' \
+            ORDER BY states.id ASC".format(arg[4]))
     data = cur.fetchall()
     for i in data:
         print(i)
-    conn.close()
-    cur.close()
+        cur.close()
+        db.close()
