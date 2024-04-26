1e9317ecf48d0682c644462a3918154827b96d30
hadeyeenkah <tboysammy@gmail.com>
2024-04-26 09:24:24 -0700

    mysqldb

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,32 +1,27 @@
 #!/usr/bin/python3
 """
-This script prints all City objects
-from the database `hbtn_0e_14_usa`.
+This script changes the name of a State object
+from the database `hbtn_0e_6_usa`.
 """
 
 from sys import argv
-from relationship_state import Base, State
-from relationship_city import City
+from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
     """
-    Access to the database and get the cities
-    from the database.
+    Updates a State object on the database.
     """
 
     db_uri = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
             argv[1], argv[2], argv[3])
     engine = create_engine(db_uri)
-    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
 
     session = Session()
-    cal_state = State(name='California')
-    sfr_city = City(name='San Francisco')
-    cal_state.cities.append(sfr_city)
 
-    session.add(cal_state)
+    ari_state = session.query(State).filter(State.id == '2').first()
+    ari_state.name = 'New Mexico'
     session.commit()
     session.close()
--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -5,8 +5,8 @@ from the database `hbtn_0e_14_usa`.
 """
 
 from sys import argv
-from relationship_state import Base, State
-from relationship_city import City
+from model_state import Base, State
+from model_city import City
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
@@ -19,14 +19,14 @@ if __name__ == "__main__":
     db_uri = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
             argv[1], argv[2], argv[3])
     engine = create_engine(db_uri)
-    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
 
     session = Session()
-    cal_state = State(name='California')
-    sfr_city = City(name='San Francisco')
-    cal_state.cities.append(sfr_city)
 
-    session.add(cal_state)
+    query = session.query(City, State).join(State)
+
+    for _c, _s in query.all():
+        print("{}: ({:d}) {}".format(_s.name, _c.id, _c.name))
+
     session.commit()
     session.close()
